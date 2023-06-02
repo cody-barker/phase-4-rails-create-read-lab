fdab2812182322b73df799640bf8b5c2eb8f1bbc
Cody <codybarker.or@gmail.com>
2023-06-02 10:36:06 -0700

    s

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -7,12 +7,13 @@ class PlantsController < ApplicationController
     end
 
     def show
-        plant = Plant.find_by(params[:id])
+        plant = Plant.find_by(id: params[:id])
         render json: plant
     end
 
     def create
         plant = Plant.create(plant_params)
+        render json: plant, status: :created
     end
 
     private
